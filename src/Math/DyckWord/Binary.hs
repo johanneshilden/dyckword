2ef273c26d3d970167ec7e9dde2f5ac778c50065
Johannes Hilden <johannes@isomorphic.co>
2017-05-01 00:16:46 +0300

    Update some haddock blocks.

--- src/Math/DyckWord/Binary.hs
+++ src/Math/DyckWord/Binary.hs
@@ -16,15 +16,15 @@
 -- some examples of which are @()()()@, @(())((()))@, and @((()()))()@.
 --
 -- The type of Dyck language considered here is defined over a binary alphabet. 
--- For concreteness, we will take this alphabet to be the set &#931; = {(, )}
--- in the following examples. The binary Dyck language is the subset of &#931;* 
--- (the Kleene closure of &#931;) of all words that satisfy two conditions:
+-- We will take this alphabet to be the set &#931; = {(, )} in the following 
+-- examples. The binary Dyck language is the subset of &#931;* (the Kleene 
+-- closure of &#931;) of all words that satisfy two conditions:
 -- 
 -- 1. The number of left brackets must be the same as the number of right 
 --    brackets.
 -- 2. Going from left to right, for each character read, the total number of 
 --    right brackets visited must be less than or equal to the number of left 
---    brackets up to the same position.
+--    brackets up to the current position.
 --
 -- E.g., @(()(()@ and @())(())()@ are __not__ Dyck words.
 --
@@ -149,7 +149,7 @@ data DyckWord = DyckWord
   } deriving (Show)
 
 -- | Two Dyck words are considered equal when they have the same /absolute/
---   rank. E.g.;
+--   rank. 
 -- 
 -- >>> fromText' "010011000111" == fromText' "xyxxyyxxxyyy"
 -- True
@@ -305,7 +305,7 @@ sizeOffs n x
 
 -- | Return the /n/-th Dyck word, restricted to only words of a given size.
 --   Words are lexicographically ordered. The result is wrapped in a 'Maybe', 
---   and 'Nothing' if the given rank is outside of the valid range. 
+--   and is equal to 'Nothing' if the given rank is outside of the valid range. 
 unrankRelative :: Size -> Rank -> Maybe DyckWord 
 unrankRelative s r 
     | r < 0 
