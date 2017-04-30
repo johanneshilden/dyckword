767e55ac663fa7ac4d2b0d4c417578cc21debeae
Johannes Hilden <johannes@isomorphic.co>
2017-04-30 14:12:30 +0300

    Prepare to clean up tests.

--- tests/Suite.hs
+++ tests/Suite.hs
@@ -11,43 +11,49 @@ import Math.DyckWord.Binary.Internal
 
 import qualified Data.Text as T
 
-testWordsOfSize :: SpecWith ()
-testWordsOfSize =
-    describe "wordsOfSize" $ do
-      it "\nreturns a list of the same length as the corresponding Catalan number" $ do
-        forM_ ([0..10] ++ [12, 14, 15]) $ 
-          \n -> do
-            putStr $ (show n) ++ " "
-            length (wordsOfSize n) `shouldBe` (catalan n)
-
-testConcat :: SpecWith ()
-testConcat = 
-    describe "concatWords" $ do
-      check "()()()" "(())" "()()()(())"
-      check "" "(())()(())" "(())()(())"
-      check "(())()((()))" "" "(())()((()))"
-      check "(())" "()()()" "(())()()()" 
-      check "()" "()" "()()" 
-  where
-    s "" = "\0949"
-    s t  = t
-    check a b c = do
-        it (T.unpack ("returns " <> s c <> " for " <> s a <> " <> " <> s b)) $
-          sum `shouldBe` word
-        it "" $
-          (size sum) `shouldBe` (size word)
-      where
-        sum  = (fromText' a) <> (fromText' b)
-        word = fromText' c
-
-testRank :: SpecWith ()
-testRank = 
-    describe "rank" $ do
-      it "..." $ 
-        rank (unrank 10) `shouldBe` 10
-
 main :: IO ()
-main = hspec $ do
-    testWordsOfSize
-    testConcat
-    testRank
+main = do
+
+    return ()
+
+
+--testWordsOfSize :: SpecWith ()
+--testWordsOfSize =
+--    describe "wordsOfSize" $ do
+--      it "\nreturns a list of the same length as the corresponding Catalan number" $ do
+--        forM_ ([0..10] ++ [12, 14, 15]) $ 
+--          \n -> do
+--            putStr $ (show n) ++ " "
+--            length (wordsOfSize n) `shouldBe` (catalan n)
+--
+--testConcat :: SpecWith ()
+--testConcat = 
+--    describe "concatWords" $ do
+--      check "()()()" "(())" "()()()(())"
+--      check "" "(())()(())" "(())()(())"
+--      check "(())()((()))" "" "(())()((()))"
+--      check "(())" "()()()" "(())()()()" 
+--      check "()" "()" "()()" 
+--  where
+--    s "" = "\0949"
+--    s t  = t
+--    check a b c = do
+--        it (T.unpack ("returns " <> s c <> " for " <> s a <> " <> " <> s b)) $
+--          sum `shouldBe` word
+--        it "" $
+--          (size sum) `shouldBe` (size word)
+--      where
+--        sum  = (fromText' a) <> (fromText' b)
+--        word = fromText' c
+--
+--testRank :: SpecWith ()
+--testRank = 
+--    describe "rank" $ do
+--      it "..." $ 
+--        rank (unrank 10) `shouldBe` 10
+--
+--main :: IO ()
+--main = hspec $ do
+--    testWordsOfSize
+--    testConcat
+--    testRank
