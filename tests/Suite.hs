ce8ad197420facb400c72583e639037d07b1e41d
Johannes Hilden <johannes@isomorphic.co>
2017-04-30 10:36:24 +0300

    Create test for wordsOfSize.

--- tests/Suite.hs
+++ tests/Suite.hs
@@ -1,14 +1,18 @@
 module Main where
 
+import Control.Monad ( forM_ )
 import Test.Hspec
 import System.Exit ( exitFailure )
 
 import Math.DyckWord.Binary
+import Math.DyckWord.Binary.Internal
 
+testWordsOfSize :: SpecWith ()
 testWordsOfSize =
     describe "wordsOfSize" $ do
-        it "returns a list of the same lenght as the corresponding Catalan number" $ do
-            length (wordsOfSize 2) `shouldBe` (2 :: Int)
+      it "returns a list of the same lenght as the corresponding Catalan number" $ do
+        forM_ ([0..10] ++ [12, 14]) $ 
+          \n -> length (wordsOfSize n) `shouldBe` (catalan n)
 
 main :: IO ()
 main = hspec $ do
